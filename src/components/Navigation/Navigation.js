c2277f072908054776b9d02dad36db99448731a1
sarah18 <sarah@mediaplusadvertising.com>
2020-02-13 16:27:03 -0500

    created the navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../logo.svg';
 
 const Navigation = () => {
   return (
-    <div>
-      <h3>Navbar here</h3>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark px-sm-5 bg-primary">
+      {/* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative Commons (Attribution 3.0 Unported); https://www.iconfinder.com/Makoto_msk */}
+
+      <Link to="/">
+        <img src={logo} alt="store" className="navbar-brand" />
+      </Link>
+
+      <ul className="navbar-nav align-items-center">
+        <li className="nav-item ml-5">
+          <Link to="/" className="nav-link">
+            products
+          </Link>
+        </li>
+      </ul>
+      <Link to="/cart" className="ml-auto">
+        <button>
+          <i className="fa fa-cart-plus">my cart</i>
+        </button>
+      </Link>
+      
+    </nav>
   )
 }
 
