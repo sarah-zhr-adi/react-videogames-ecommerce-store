6956918039015197f7b94b2bbd2ae8a662e6213c
sarah18 <sarah@mediaplusadvertising.com>
2020-02-13 16:52:32 -0500

    Added styled components to the project

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../logo.svg';
+import styled from 'styled-components';
+import { ButtonContainer } from '../../styled-components/Button';
 
 const Navigation = () => {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark px-sm-5 bg-primary">
+    <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
       {/* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative Commons (Attribution 3.0 Unported); https://www.iconfinder.com/Makoto_msk */}
 
       <Link to="/">
@@ -19,13 +21,24 @@ const Navigation = () => {
         </li>
       </ul>
       <Link to="/cart" className="ml-auto">
-        <button>
-          <i className="fa fa-cart-plus">my cart</i>
-        </button>
+        <ButtonContainer>
+          <span className="mr-2">
+            <i className="fa fa-cart-plus"></i>
+          </span>
+          my cart
+        </ButtonContainer>
       </Link>
-      
-    </nav>
+    </NavWrapper>
   )
 }
+
+const NavWrapper = styled.nav `
+  background-color: var(--mainOrange);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`
 
 export default Navigation;
