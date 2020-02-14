ee0b3f07ff532761cff7e7e37bad441293503463
sarah18 <sarah@mediaplusadvertising.com>
2020-02-14 14:43:54 -0500

    Added context api

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Product from '../Product/Product';
+import Title from './../Title/Title';
+import { storeProducts } from '../../data';
 
-const ProductList = () => {
-  return (
-    <div>
-      <Product/>
-    </div>
-  )
+
+
+class ProductList extends Component {
+  state = {
+    products: storeProducts
+  };
+  render(){
+    console.log(this.state.products);
+    return (
+      <>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+
+            </div>
+          </div>
+        </div>
+      </>
+    )
+    // <Product/>
+  }
 }
 
 export default ProductList;
