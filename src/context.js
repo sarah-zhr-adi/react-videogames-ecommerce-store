15aa8ef67a83e945ca4b043486885637e22293e9
Sarah Adi <sarah.zohour.adi@gmail.com>
2020-04-08 20:35:55 -0400

    added increment and decrement to product quantity in cart

--- src/context.js
+++ src/context.js
@@ -81,16 +81,51 @@ class ProductProvider extends Component {
   }
 
   increment = (id) => {
-    console.log("increment method");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item=> item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count + 1;
+    product.total = product.count * product.price;
+
+    this.setState(()=> {
+        return {
+          cart: [...tempCart]
+        }
+      },
+      ()=>{
+        this.addTotal();
+      }
+    )
   }
 
   decrement = (id) => {
-    console.log("decrement method");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item=> item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count - 1;
+    if(product.count === 0) {
+      this.removeItem(id);
+    } else {
+        product.total = product.count * product.price;
+        this.setState(()=> {
+          return {
+            cart: [...tempCart]
+          }
+        },
+        ()=>{
+          this.addTotal();
+        }
+      )
+    }
+    
+    
   }
 
   removeItem = (id) => {
     let tempProducts = [...this.state.products];
-    let tempCart = [...this.state.cart];
+    let tempCart = [...this.state.cart]; 
 
     tempCart = tempCart.filter(item => item.id !== id);
 
